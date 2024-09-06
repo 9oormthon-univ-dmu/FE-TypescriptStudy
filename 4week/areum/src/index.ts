28653cbf34e047da60958518b2667fdd143b73f6
areum117 <qnfmfmd1@gmail.com>
2024-09-06 11:45:40 +0900

    5.6 Definitely Typed

--- 4week/areum/src/index.ts
+++ 4week/areum/src/index.ts
@@ -1,1 +1,21 @@
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    //블록 생성
+    constructor(public prevHash: string, public height: number, public data: string) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash: string, height: number, data: string) {
+        //해시 생성
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
