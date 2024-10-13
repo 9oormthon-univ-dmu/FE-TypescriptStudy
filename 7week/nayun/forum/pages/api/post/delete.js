825daddc2838c0d8f3faa280406df8d2cd4ebf78
unknown <nayunk10@gmail.com>
2024-10-13 13:47:35 +0900

    8week

--- 7week/nayun/forum/pages/api/post/delete.js
+++ 7week/nayun/forum/pages/api/post/delete.js
@@ -2,9 +2,10 @@ import { connectDB } from "@/util/database.js";
 import { ObjectId } from "mongodb";
 
 export default async function handler(request, response) {
+  const { _id } = JSON.parse(request.body);
   if (request.method == "DELETE") {
     let db = (await connectDB).db("forum");
-    let result = await db.collection("post").deleteOne({_id : new ObjectId(request.body._id)});
+    let result = await db.collection("post").deleteOne({_id : new ObjectId(_id)});
     console.log(result);
 
     response.status(200).json('delete');
